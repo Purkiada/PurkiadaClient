15aee09b54678249872347fb4af4a37ab3c61a6a
Lukáš Matuška <lukynmatuska@gmail.com>
2021-11-09 17:35:42 +0100

    Improve environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  backend: {
+    app: "https://api.purkiada.cz/"
+  },
+  keycloak: {
+    issuer: 'https://id.matejbucek.cz/auth/realms/Purkiada',
+    redirectUri: 'https://purkiada.cz/',
+    clientId: 'frontend',
+    responseType: 'code',
+    scope: 'openid profile email',
+    requireHttps: true,
+    showDebugInformation: true,
+    disableAtHashCheck: true
+  }
 };
