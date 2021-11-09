c99413d3c6940d7f9de9e558e10910b28b4290cd
Matěj Bucek <admin@mbucek.cz>
2021-11-09 17:35:36 +0100

    PurkiadaClient: fix.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  backend: {
+    app: "https://api.purkiada.cz"
+  },
+  keycloak: {
+    issuer: 'https://id.matejbucek.cz/auth/realms/Purkiada',
+    redirectUri: 'http://localhost:4200',
+    clientId: 'frontend',
+    responseType: 'code',
+    scope: 'openid profile email',
+    requireHttps: true,
+    showDebugInformation: true,
+    disableAtHashCheck: true
+  }
 };
